667e393c84336d171542d8247ad1077fe367c1a2
Cayce House <cayce@houseco.cc>
2018-09-04 12:14:23 -0400

    Fixed scrape every 5 minutes.

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -5,7 +5,7 @@ namespace App\Console;
 use Illuminate\Console\Scheduling\Schedule;
 use Illuminate\Foundation\Console\Kernel as ConsoleKernel;
 
-use App\Labtechs;
+use App\Jobs\GetNewTickets;
 
 class Kernel extends ConsoleKernel
 {
@@ -26,7 +26,7 @@ class Kernel extends ConsoleKernel
      */
     protected function schedule(Schedule $schedule)
     {
-        $schedule->job(new Labtechs)->everyFiveMinutes();
+        $schedule->job(new GetNewTickets)->everyFiveMinutes();
     }
 
     /**
