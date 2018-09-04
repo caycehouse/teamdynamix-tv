c7f7428c75a4bd22143429dccc7c3b0d4610aaf0
Cayce House <cayce@houseco.cc>
2018-09-04 12:43:54 -0400

    Tickets portion implemented.

---
+++ app/Http/Controllers/DashboardController.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Ticket;
+use App\Http\Controllers\Controller;
+
+class DashboardController extends Controller
+{
+    /**
+     * Show the dashboard.
+     *
+     * @return Response
+     */
+    public function __invoke()
+    {
+        $tickets = Ticket::whereNotIn('status', ['Closed', 'Cancelled'])->orderBy('ticket_created_at', 'desc')->get();
+
+        return view('dashboard.index', ['tickets' => $tickets]);
+    }
+}
---
+++ resources/views/dashboard/index.blade.php
@@ -0,0 +1,48 @@
+<!doctype html>
+<html lang="{{ app()->getLocale() }}">
+<head>
+  <meta charset="utf-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+
+  <!-- CSRF Token -->
+  <meta name="csrf-token" content="{{ csrf_token() }}">
+
+  <title>{{ config('app.name', 'Laravel') }}</title>
+
+  <!-- Styles -->
+  <link href="{{ asset('css/app.css') }}" rel="stylesheet">
+</head>
+<body>
+  <div class="container-fluid">
+    <div class="row">
+      <div class="col">
+      </div>
+      <div class="col">
+        <table class="table table-sm">
+          <thead>
+            <th>ID</th>
+            <th>Title</th>
+            <th>Lab</th>
+            <th>Status</th>
+            <th>Created</th>
+          </thead>
+          <tbody>
+            @foreach($tickets as $ticket)
+              <tr>
+                <td>{{ $ticket->ticket_id }}</td>
+                <td>{{ $ticket->title }}</td>
+                <td>{{ $ticket->lab }}</td>
+                <td>{{ $ticket->status }}</td>
+                <td>{{ $ticket->ticket_created_at }}</td>
+              </tr>
+            @endforeach
+          </tbody>
+        </table>
+      </div>
+    </div>
+  </div>
+  <!-- Scripts -->
+  <script src="{{ asset('js/app.js') }}"></script>
+</body>
+</html>
