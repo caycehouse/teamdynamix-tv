3ea4847286e93ad77817f5d5cc212d7682dc59d5
Cayce House <cayce@houseco.cc>
2018-09-25 12:19:51 -0400

    Cleaned up GetTickets

--- app/Console/Commands/GetTickets.php
+++ app/Console/Commands/GetTickets.php
@@ -19,7 +19,7 @@ class GetTickets extends Command
      *
      * @var string
      */
-    protected $description = 'Gets new tickets from TeamDynamix.';
+    protected $description = 'Gets new tickets from TeamDynamix';
 
     /**
      * Create a new command instance.
