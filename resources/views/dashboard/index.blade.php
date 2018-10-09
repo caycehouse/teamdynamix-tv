d6b76969cae032125d9e4cb3d13caf7b31d655b7
Cayce House <cayce@houseco.cc>
2018-10-09 10:14:06 -0400

    Set better styling

--- resources/views/dashboard/index.blade.php
+++ resources/views/dashboard/index.blade.php
@@ -14,7 +14,7 @@
   <link href="{{ asset('css/app.css') }}" rel="stylesheet">
 </head>
 <body>
-    <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
+    <nav class="navbar navbar-expand-lg navbar-dark" style="background-color: #592a8a;">
         <a class="navbar-brand text-warning" href="#">Labtechs TV</a>
         <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
             <span class="navbar-toggler-icon"></span>
