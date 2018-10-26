20d64a3a8840efa4b47db477d077f8d506c6bbd9
Cayce House <cayce@houseco.cc>
2018-10-26 12:00:42 -0400

    Added Deployer

---
+++ deploy.php
@@ -0,0 +1,41 @@
+<?php
+namespace Deployer;
+
+require 'recipe/laravel.php';
+
+// Project name
+set('application', 'Labtechs TV');
+
+// Project repository
+set('repository', 'https://github.com/caycehouse/labtechs-tv-laravel.git');
+
+// [Optional] Allocate tty for git clone. Default value is false.
+set('git_tty', true);
+
+// Shared files/dirs between deploys
+add('shared_files', []);
+add('shared_dirs', []);
+
+// Writable dirs by web server
+add('writable_dirs', []);
+set('allow_anonymous_stats', false);
+
+// Hosts
+
+host(getenv('host'))
+    ->user(getenv('user'))
+    ->set('deploy_path', '~/td.sumojoe.com');
+
+// Tasks
+
+task('build', function () {
+    run('cd {{release_path}} && build');
+});
+
+// [Optional] if deploy fails automatically unlock.
+after('deploy:failed', 'deploy:unlock');
+
+// Migrate database before symlink new release.
+
+before('deploy:symlink', 'artisan:migrate');
+
