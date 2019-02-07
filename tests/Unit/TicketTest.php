cecb4b4db95b968eb1188a56631be20f296c0177
Cayce House <cayce@houseco.cc>
2019-02-07 15:43:38 -0500

    Added test for unresolved tickets

--- tests/Unit/TicketTest.php
+++ tests/Unit/TicketTest.php
@@ -41,4 +41,35 @@ class TicketTest extends TestCase
         $this->assertTrue($isResolved);
         $this->assertEquals(Ticket::resolved()->count(), 2);
     }
+
+    /**
+     * Tests the resolved ticket scope.
+     *
+     * @return void
+     */
+    public function testGettingOnlyUnresolvedTickets()
+    {
+        $faker = Factory::create();
+
+        // Resolved Tickets
+        factory(Ticket::class)->create(['status' => 'Closed']);
+        factory(Ticket::class)->create(['status' => 'Cancelled']);
+
+        // Unresolved Tickets
+        factory(Ticket::class)->create(['status' => 'Work-in-Progress']);
+        factory(Ticket::class)->create(['status' => 'New']);
+        factory(Ticket::class)->create(['status' => 'On Hold']);
+
+        $isUnresolved = true;
+        foreach (Ticket::unresolved()->get() as $ticket) {
+            $isUnresolved = ('New' === $ticket->status || 'Work-in-Progress' === $ticket->status || 'On Hold' === $ticket->status);
+
+            if (! $isUnresolved) {
+                break;
+            }
+        }
+
+        $this->assertTrue($isUnresolved);
+        $this->assertEquals(Ticket::unresolved()->count(), 3);
+    }
 }
