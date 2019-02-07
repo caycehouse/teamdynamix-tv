cf9374bbb9dee37501beba5877e31f6b9094c1c4
Cayce House <cayce@houseco.cc>
2019-02-07 17:42:53 -0500

    Added redis facade to update tickets

--- app/Jobs/UpdateTickets.php
+++ app/Jobs/UpdateTickets.php
@@ -8,6 +8,7 @@ use Illuminate\Contracts\Queue\ShouldQueue;
 use Illuminate\Foundation\Bus\Dispatchable;
 use Illuminate\Queue\InteractsWithQueue;
 use Illuminate\Queue\SerializesModels;
+use Illuminate\Support\Facades\Redis;
 
 class UpdateTickets implements ShouldQueue
 {
