762f1d17ca62f8cf2b816366bfdc401c1e4a2de1
Cayce House <cayce@houseco.cc>
2019-03-13 18:25:57 -0400

    Removed poison.

--- mix.exs
+++ mix.exs
@@ -43,8 +43,7 @@ defmodule TeamdynamixTv.MixProject do
       {:gettext, "~> 0.11"},
       {:jason, "~> 1.0"},
       {:plug_cowboy, "~> 2.0"},
-      {:httpoison, "~> 1.5"},
-      {:poison, "~> 3.1"}
+      {:httpoison, "~> 1.5"}
     ]
   end
 
