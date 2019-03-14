0c8755056e976dcdce797c021ec4884168244e2e
Cayce House <cayce@houseco.cc>
2019-03-13 20:06:23 -0400

    Added ticket_id as unique constraint

--- lib/teamdynamix_tv/ticket.ex
+++ lib/teamdynamix_tv/ticket.ex
@@ -18,5 +18,6 @@ defmodule TeamdynamixTv.Ticket do
     ticket
     |> cast(attrs, [:ticket_id, :title, :status, :days_old, :resp_group])
     |> validate_required([:ticket_id, :title, :status, :days_old, :resp_group])
+    |> unique_constraint(:ticket_id)
   end
 end
