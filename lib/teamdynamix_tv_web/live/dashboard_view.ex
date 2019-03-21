adcd1db402b1ef5a09ab8ddf7a4ebde05c9477aa
Cayce House <cayce@houseco.cc>
2019-03-21 14:51:39 -0400

    Cleaned up dashboard view render

--- lib/teamdynamix_tv_web/live/dashboard_view.ex
+++ lib/teamdynamix_tv_web/live/dashboard_view.ex
@@ -5,7 +5,7 @@ defmodule TeamdynamixTvWeb.DashboardView do
     TeamdynamixTvWeb.PageView.render("index.html", assigns)
   end
 
-  def mount(%{resp_group: resp_group}, socket) do
+  def get_tickets(resp_group) do
     # Imports only from/2 of Ecto.Query
     import Ecto.Query, only: [from: 2]
 
@@ -15,27 +15,18 @@ defmodule TeamdynamixTvWeb.DashboardView do
               select: [:ticket_id, :title, :status, :days_old],
               order_by: t.days_old
 
-    tickets = TeamdynamixTv.Repo.all(query)
+    tickets = TeamdynamixTv.Repo.all(query)   
+  end
 
+  def mount(%{resp_group: resp_group}, socket) do
     if connected?(socket), do: Process.send_after(self(), :tick, 1000)
-    {:ok, assign(socket, tickets: tickets, resp_group: resp_group)}
+    {:ok, assign(socket, tickets: get_tickets(resp_group), resp_group: resp_group)}
   end
 
   def handle_info(:tick, socket) do
     resp_group = socket.assigns.resp_group
 
-    # Imports only from/2 of Ecto.Query
-    import Ecto.Query, only: [from: 2]
-
-    # Create a query
-    query = from t in "tickets",
-              where: t.resp_group == type(^resp_group, :string),
-              select: [:ticket_id, :title, :status, :days_old],
-              order_by: t.days_old
-
-    tickets = TeamdynamixTv.Repo.all(query)
-
     Process.send_after(self(), :tick, 1000)
-    {:noreply, assign(socket, tickets: tickets)}
+    {:noreply, assign(socket, tickets: get_tickets(resp_group))}
   end
 end
