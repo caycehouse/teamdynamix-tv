7dd837c50fbbcf5434456f6b5ee4935ef3d9e340
Cayce House <cayce@mobilemedics.com>
2019-08-30 08:51:11 -0400

    Fixed start.sh

--- start.sh
+++ start.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
-
+  
 # Initial setup
 MIX_ENV=prod mix deps.get --only prod
 MIX_ENV=prod mix compile
 
 # Compile assets
-cd assets && webpack --mode production && cd ..
+cd assets && npm i && webpack --mode production && cd ..
 
 # Create our digest
 MIX_ENV=prod mix phx.digest
