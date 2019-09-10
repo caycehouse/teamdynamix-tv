3c937e5827e1a6d578c87d43e1996357217499a8
Cayce House <cayce@mobilemedics.com>
2019-09-10 09:58:28 -0400

    Changed app version to 1.0

--- mix.exs
+++ mix.exs
@@ -4,7 +4,7 @@ defmodule TeamdynamixTv.MixProject do
   def project do
     [
       app: :teamdynamix_tv,
-      version: "0.1.0",
+      version: "1.0.0",
       elixir: "~> 1.5",
       elixirc_paths: elixirc_paths(Mix.env()),
       compilers: [:phoenix, :gettext] ++ Mix.compilers(),
