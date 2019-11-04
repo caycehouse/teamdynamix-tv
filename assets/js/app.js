2e25781e677b8b4e196bc0ac15d60fa68e2c9f86
Cayce House <cayce@mobilemedics.com>
2019-11-04 10:59:10 -0500

    Fixed live socket

--- assets/js/app.js
+++ assets/js/app.js
@@ -18,6 +18,7 @@ import "phoenix_html";
 // import socket from "./socket"
 
 import LiveSocket from "phoenix_live_view";
+import { Socket } from "phoenix";
 
-let liveSocket = new LiveSocket("/live");
+let liveSocket = new LiveSocket("/live", Socket);
 liveSocket.connect();
