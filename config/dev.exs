6fb5bfb9bd9219ac691b6b308a2b6fc93d0f5005
Cayce House <cayce@mobilemedics.com>
2019-11-04 11:09:49 -0500

    Updated dev secret

--- config/dev.exs
+++ config/dev.exs
@@ -76,4 +76,6 @@ config :phoenix, :stacktrace_depth, 20
 # Initialize plugs at runtime for faster development compilation
 config :phoenix, :plug_init_mode, :runtime
 
+# Finally import the config/dev.secret.exs which loads secrets
+# and configuration from environment variables.
 import_config "dev.secret.exs"
