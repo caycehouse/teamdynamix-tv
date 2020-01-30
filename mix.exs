142b0cc43ce1ed95048c687917e504f0179e9b29
Cayce House <cayce@mobilemedics.com>
2020-01-30 10:42:55 -0500

    Bump mix deps

--- mix.exs
+++ mix.exs
@@ -33,9 +33,9 @@ defmodule TeamdynamixTv.MixProject do
   # Type `mix help deps` for examples and options.
   defp deps do
     [
-      {:phoenix, "~> 1.4.9"},
+      {:phoenix, "~> 1.4.12"},
       {:phoenix_pubsub, "~> 1.1"},
-      {:phoenix_ecto, "~> 4.0"},
+      {:phoenix_ecto, "~> 4.1"},
       {:ecto_sql, "~> 3.1"},
       {:postgrex, ">= 0.0.0"},
       {:phoenix_html, "~> 2.11"},
