24fde0ef0abfd2472e2d34a29243b4920488b435
Cayce House <cayce@mobilemedics.com>
2020-06-23 01:56:21 -0400

    Update router.ex

--- lib/teamdynamix_tv_web/router.ex
+++ lib/teamdynamix_tv_web/router.ex
@@ -7,7 +7,7 @@ defmodule TeamdynamixTvWeb.Router do
     plug :fetch_flash
     plug :protect_from_forgery
     plug :put_secure_browser_headers
-    plug Phoenix.LiveView.Flash
+    plug :fetch_live_flash
   end
 
   pipeline :api do
