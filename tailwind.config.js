033d8599908448e75247e0b5ea8de4e11ecb3ede
Cayce House <cayce@mobilemedics.com>
2021-09-20 18:26:35 +0000

    Fixed tree shaking

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,27 +1,31 @@
 const defaultTheme = require('tailwindcss/defaultTheme');
 
 module.exports = {
-    purge: [
-        './vendor/laravel/framework/src/Illuminate/Pagination/resources/views/*.blade.php',
-        './vendor/laravel/jetstream/**/*.blade.php',
-        './storage/framework/views/*.php',
-        './resources/views/**/*.blade.php',
-    ],
+    mode: 'jit',
+    purge: {
+        content: [
+            './vendor/laravel/framework/src/Illuminate/Pagination/resources/views/*.blade.php',
+            './vendor/laravel/jetstream/**/*.blade.php',
+            './storage/framework/views/*.php',
+            './resources/views/**/*.blade.php',
+        ],
+        safelist: [
+            'bg-red-100',
+            'border',
+            'border-red-400',
+            'text-red-700',
+            'bg-yellow-100',
+            'border-yellow-400',
+            'text-yellow-700',
+            'text-gray-400'
+        ]
+    },
 
     theme: {
         extend: {
             fontFamily: {
                 sans: ['Nunito', ...defaultTheme.fontFamily.sans],
             },
-            screens: {
-                'print': {'raw': 'print'},
-            },
-        },
-    },
-
-    variants: {
-        extend: {
-            opacity: ['disabled'],
         },
     },
 };
